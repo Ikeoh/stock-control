d848c8f3e39062384020a203c2cb55f0a706ff0f
Ikeoh <newton.negri@hotmail.com>
2023-12-27 17:05:30 -0300

    27/12/2023

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'stock-control';
+
+  constructor(
+    private primengConfig: PrimeNGConfig
+  ) { }
+
+  ngOnInit(): void {
+    this.primengConfig.ripple = true
+  }
+
 }
