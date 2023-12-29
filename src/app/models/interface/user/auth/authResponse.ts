3f445b32f7aa4d7dd4ff96fcf12f958315058d18
newton.negri <newton.negri@hotmail.com>
2023-12-29 20:25:53 -0300

    29/12/2023

--- src/app/models/interface/user/auth/authResponse.ts
+++ src/app/models/interface/user/auth/authResponse.ts
@@ -1,5 +1,6 @@
-export interface signupUserResponse {
+export interface authResponse {
   id: string;
   name: string;
   email: string;
+  token: string;
 }
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -4,6 +4,8 @@ import { enviroment } from '../../../enviroments/enviroment';
 import { Observable } from 'rxjs';
 import { signupUserRequest } from '../../models/interface/user/signupUserRequest';
 import { signupUserResponse } from '../../models/interface/user/signupUserResponse';
+import { authRequest } from '../../models/interface/user/auth/authRequest';
+import { authResponse } from '../../models/interface/user/auth/authResponse';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +18,14 @@ export class UserService {
     private http: HttpClient
   ) { }
 
+
+  //Método que faz a requisição de login no backend e retorna os dados.
   signupUser(requestDatas: signupUserRequest): Observable<signupUserResponse> {
-    return this.http.post<signupUserResponse>(
-      `${}`
-    )
+    return this.http.post<signupUserResponse>(`${this.API_URL}/user`, requestDatas)
+  }
+
+  //Método que faz a autenticação do login no backend e retorna os dados.
+  authUser(requestDatas: authRequest): Observable<authResponse> {
+    return this.http.post<authResponse>(`${this.API_URL}/auth`, requestDatas)
   }
 }
