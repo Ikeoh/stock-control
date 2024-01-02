469d9b0cd45990c0522bfe4467850767186c4694
Ikeoh <newton.negri@hotmail.com>
2024-01-02 15:51:51 -0300

    02/01/2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { PrimeNGConfig } from 'primeng/api';
-import {CardModule} from 'primeng/card';
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
-import {ToastModule} from 'primeng/toast';
 
 
 
@@ -18,19 +12,11 @@ import {ToastModule} from 'primeng/toast';
   imports: [
     CommonModule,
     RouterOutlet,
-    //HttpClientModule,
-    //ReactiveFormsModule,
-
-    //Imports do PrimeNG
-      //CardModule,
-      //InputTextModule,
-      //ButtonModule,
-      //ToastModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'stock-control';
 
   constructor(
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { UserService } from '../../services/user/user.service';
+import { signupUserRequest } from '../../models/interface/user/signupUserRequest';
+import { authRequest } from '../../models/interface/user/auth/authRequest';
 
 @Component({
   selector: 'app-home',
@@ -26,18 +30,44 @@ export class HomeComponent {
 
   signupForm = this.formBuilder.group({
     name: ['', Validators.required],
-    email: [ '', Validators.required],
+    email: ['', Validators.required],
     password: ['', Validators.required],
   })
 
-  constructor(private formBuilder: FormBuilder) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private cookieService: CookieService,
+  ) { }
 
   onSubmitLoginForm(): void {
-    console.log('DADOS DO FORMULÁRIO DE LOGIN', this.loginForm.value);
+    if (this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as authRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              this.cookieService.set('USER_INFO', response?.token);
+              this.loginForm.reset();
+            }
+          },
+          error: (err) => console.log(err),
+        });
+    }
   }
+
   onSubmitSignupForm(): void {
-    console.log('DADOS DE CRIAÇÃO DE CONTA', this.signupForm.value);
+    if (this.signupForm.value && this.signupForm.valid) {
+      this.userService.signupUser(this.signupForm.value as signupUserRequest)
+        .subscribe({
+          next: (response) => {
+            if (response) {
+              alert('Usuário criado com sucesso!');
+              this.signupForm.reset();
+              this.loginCard = true;
+            }
+          },
+          error: (err) => console.log(err),
+        });
+    }
   }
-
-
 }
