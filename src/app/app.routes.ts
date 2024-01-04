85d5b47d262bf9f9ac29e31560f6fc68c9599d96
Ikeoh <newton.negri@hotmail.com>
2024-01-04 18:02:33 -0300

    04/01/2024

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home.component';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
     component: HomeComponent,
-  }
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
 ];
